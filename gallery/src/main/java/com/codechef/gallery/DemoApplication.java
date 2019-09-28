b5b671e412f37a2fe1429608b745b64180ee20c2
diwanshu <csediwanshu@gmail.com>
2019-09-28 22:38:07 +0530

    Initial Commit

---
+++ gallery/src/main/java/com/codechef/gallery/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.codechef.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ gallery/src/main/java/com/codechef/service/ServiceTest.java
@@ -0,0 +1,1 @@
+package com.codechef.service;
